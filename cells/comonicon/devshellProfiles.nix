ac6cf5a7907c1263393613f4bf161e8d64cffa33
GTrunSec <gtrunsec@hardenedlinux.org>
2022-02-15 00:21:38 -0800

    fix: mycmd cells's path

--- cells/comonicon/devshellProfiles.nix
+++ cells/comonicon/devshellProfiles.nix
@@ -16,7 +16,8 @@ in
       {
         name = "mycmd";
         command = ''
-        julia -e "import Pkg; Pkg.activate(\"$CELL_ROOT/comonicon\"); Pkg.instantiate()" -L mycmd.jl -- $@
+        cd $CELL_ROOT/comonicon
+        julia -e "import Pkg; Pkg.activate(\".\"); Pkg.instantiate()" -L mycmd.jl -- $@
         '';
         category = "julia";
       }
