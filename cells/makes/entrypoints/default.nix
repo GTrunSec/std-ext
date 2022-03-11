74a1c7288e2a90e45e477a32e2f5a8d0c5d1ba64
GTrunSec <gtrunsec@hardenedlinux.org>
2022-03-11 00:53:12 -0800

    remove makes on darwin

--- cells/makes/entrypoints/default.nix
+++ cells/makes/entrypoints/default.nix
@@ -4,7 +4,7 @@
 }: let
   inherit (inputs) nixpkgs;
   inherit (inputs.cells._writers.library) writeShellApplication;
-  makes = inputs.std.std.lib.fromMakesWith inputs;
+  makes = inputs.std."x86_64-linux".std.lib.fromMakesWith inputs;
 in {
   secrets-for-gpg-from-env = makes ./tests/secrets-for-gpg-from-env.nix {};
   shell = writeShellApplication {
