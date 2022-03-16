08fd5416f62b432c32e2147a0e1bad11179d90ff
GTrunSec <gtrunsec@hardenedlinux.org>
2022-03-16 01:54:28 -0700

    vast.yaml: stage-1

--- cells/tenzir/entrypoints/default.nix
+++ cells/tenzir/entrypoints/default.nix
@@ -2,23 +2,34 @@
   inputs,
   cell,
 }: let
-  inherit (inputs) nixpkgs self;
+  inherit (inputs) nixpkgs self data-merge;
   inherit (cell) nomadJobs configFiles;
   inherit (inputs.cells._writers.library) writeShellApplication;
   inherit (inputs.cells.makes.library) __output__ makeSubstitution;
+  # default
+  vast-settings = {
+    # db-directory = "vast.db";
+  };
+  # template
+  vast-yaml = configFiles.vast {
+    db-directory = "vast.db";
+  };
 in {
   vast-config = let
-    vast-yaml = {
-      vast = configFiles.vast {
-        db-directory = "vast.db";
+    # state
+    prod = let
+      vast.start = {
+        print-endpoint = true;
       };
+    in data-merge.merge (configFiles.vast (vast-settings // { db-directory = "vast.db"; })) {
+      vast.print-endpoint = true;
     };
   in
     writeShellApplication {
       name = "vast.yaml";
       runtimeInputs = [nixpkgs.remarshal];
       text = let
-        json = nixpkgs.writeText "JSON" (builtins.toJSON vast-yaml);
+        json = nixpkgs.writeText "JSON" (builtins.toJSON prod);
       in ''
         json2yaml  -i ${json} -o "$@"
       '';
