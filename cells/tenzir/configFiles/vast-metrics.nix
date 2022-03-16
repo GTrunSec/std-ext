e1bac261d2251f85642348a1376299700500fa59
GTrunSec <gtrunsec@hardenedlinux.org>
2022-03-16 00:51:29 -0700

    vast.yaml: update

--- cells/tenzir/configFiles/vast-metrics.nix
+++ cells/tenzir/configFiles/vast-metrics.nix
@@ -1,7 +1,21 @@
-{...}: {
-  self-sink = {
-    enable = true;
-    slice-size = 128;
-    slice-type = "arrow";
+{db-directory, ...}: {
+  metrics = {
+    self-sink = {
+      enable = true;
+      slice-size = 128;
+      slice-type = "arrow";
+    };
+    file-sink = {
+      enable = false;
+      real-time = false;
+      path = "${db-directory}/vast-metrics.log";
+    };
+    # Configures if and where metrics should be written to a socket.
+    uds-sink = {
+      enable = false;
+      real-time = false;
+      path = "${db-directory}/vast-metrics.sock";
+      type = "datagram";
+    };
   };
 }
