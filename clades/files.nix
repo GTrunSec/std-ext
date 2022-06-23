f95bea3f20898f3c0e8d4720b513d5648cfc382a
GTrunSec <gtrunsec@hardenedlinux.org>
2022-06-22 22:44:42 -0700

    fix lib

--- clades/files.nix
+++ clades/files.nix
@@ -14,6 +14,7 @@ inputs: let
       fragmentRelPath,
     }: let
       nixpkgs = inputs.nixpkgs.legacyPackages.${system};
+      l = nixpkgs;
       builder = ["nix" "build" "--impure" "--json" "--no-link" "${flake}#${fragment}"];
       jq = ["|" "${nixpkgs.legacyPackages.${system}.jq}/bin/jq" "-r" "'.[].outputs.out'"];
       bat = ["${nixpkgs.legacyPackages.${system}.bat}/bin/bat"];
