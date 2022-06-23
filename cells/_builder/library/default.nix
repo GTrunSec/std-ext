43009d2d1865aed5b5dd71cde836f79e80e04fb5
GTrunSec <gtrunsec@hardenedlinux.org>
2022-06-22 19:54:17 -0700

    add mkPaths

---
+++ cells/_builder/library/default.nix
@@ -0,0 +1,8 @@
+{ inputs, cell }:
+let
+  inherit (inputs) nixpkgs;
+  inherit (inputs.nixpkgs) lib;
+in
+{
+  mkPaths = import ./mkPaths.nix { inherit nixpkgs lib; };
+}
---
+++ cells/_builder/library/mkPaths.nix
@@ -0,0 +1,15 @@
+{ nixpkgs,lib }:
+{
+  name,
+  paths ? []
+}:
+let
+  cpPackages = lib.concatStringsSep "\n" (map (f: let name = f.pname or f.name; in "ln -s ${f} $out/${name}") paths);
+  inherit paths;
+in
+nixpkgs.runCommand name {
+  buildInputs = [];
+} ''
+  mkdir -p $out
+ ${cpPackages}
+    ''
