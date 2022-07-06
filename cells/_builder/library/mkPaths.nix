f53ba2622b877afcecdd2dac76418e346bbc13c7
GTrunSec <gtrunsec@hardenedlinux.org>
2022-07-06 15:37:05 -0700

    Update

--- cells/_builder/library/mkPaths.nix
+++ cells/_builder/library/mkPaths.nix
@@ -7,10 +7,15 @@
 }: let
   cpPackages = lib.concatStringsSep "\n" (map (f: let name = f.pname or f.name; in "ln -s ${f} $out/${name}") paths);
   inherit paths;
+  bin = nixpkgs.symlinkJoin {
+    inherit paths;
+    name = "bin";
+  };
 in
   nixpkgs.runCommand name {
     buildInputs = [];
   } ''
      mkdir -p $out
+     ln -s ${bin}/bin $out
     ${cpPackages}
   ''
