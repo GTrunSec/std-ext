06fb9b5e8e77429429c879f7de4a2a2082bc4015
GTrunSec <gtrunsec@hardenedlinux.org>
2022-07-17 00:36:58 -0700

    fix

--- cells/update/entrypoints/nvfetcher-update.bash
+++ cells/update/entrypoints/nvfetcher-update.bash
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$@")"
 # shellcheck disable=all
-nvfetcher -c "$PRJ_ROOT/$@" -l nvfetcher-changelog
+nvfetcher -c "$PRJ_ROOT/$@" -l /tmp/nvfetcher-changelog
 # shellcheck disable=all
-if [[ -n "$(cat nvfetcher-changelog)" && -n "${GITHUB_ENV}" ]]; then
+if [[ -n "$(cat /tmp/nvfetcher-changelog)" && -n "${GITHUB_ENV}" ]]; then
      echo "COMMIT_MSG<<EOF" >>"$GITHUB_ENV"
-     echo "$(cat changelog)" >>"$GITHUB_ENV"
+     echo "$(cat /tmp/nvfetcher-changelog)" >>"$GITHUB_ENV"
      echo "EOF" >>"$GITHUB_ENV"
-    rm -rf nvfetcher-changelog
-else
-    rm -rf nvfetcher-changelog
 fi
