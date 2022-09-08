a40a84b0ef3123ba9b4f5f7eb95c49ca414dae99
GTrunSec <gtrunsec@hardenedlinux.org>
2022-09-08 04:13:59 -0700

    update

--- nix/main/library/default.nix
+++ nix/main/library/default.nix
@@ -7,13 +7,13 @@
   __inputs__ = cell.library.callFlake "${(std.incl self [(self + /lock)])}/lock" {
     # this is a hack to get the lock file to be followed in our nixpkgs channel
     nixpkgs.locked = inputs.nixpkgs-lock.sourceInfo;
-    nixos.locked = inputs.nixos.sourceInfo;
+    nixos.locked = inputs.nixos.sourceInfo // {type = "github"; owner = "NixOS"; repo = "nixpkgs";};
     statix.inputs.nixpkgs = "nixpkgs";
     nixpkgs-hardenedlinux.inputs.nixpkgs = "nixpkgs";
     nvfetcher.inputs.nixpkgs = "nixos";
   };
 in {
-  inherit __inputs__;
+  inherit __inputs__ inputs;
   /*
   tests = mergeDevShell { default = cell.devshellProfiles.mkshell; mkshell = cell.devshell;};
   */
