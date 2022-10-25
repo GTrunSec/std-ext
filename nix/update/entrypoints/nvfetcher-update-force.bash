94d89e35c8e262546289b42ecd2d8e2d3c0a34ff
GTrunSec <gtrunsec@hardenedlinux.org>
2022-10-25 00:57:14 -0700

    fix

--- nix/update/entrypoints/nvfetcher-update-force.bash
+++ nix/update/entrypoints/nvfetcher-update-force.bash
@@ -3,7 +3,7 @@ cd "$(dirname "$@")"
 # shellcheck disable=all
 nvfetcher -c "$PRJ_ROOT/$@" -l /tmp/nvfetcher-changelog
 # shellcheck disable=all
-if [[ ! -n "$(git diff HEAD --unified=0 | grep 'sha256')" && -n "$(cat /tmp/nvfetcher-changelog)" && -n "${GITHUB_ENV}" ]]; then
+if [[ -n "$(git diff HEAD --unified=0 | grep 'sha256')" && -n "$(cat /tmp/nvfetcher-changelog)" && -n "${GITHUB_ENV}" ]]; then
      echo "COMMIT_MSG<<EOF" >>"$GITHUB_ENV"
      echo "$(cat /tmp/nvfetcher-changelog)" >>"$GITHUB_ENV"
      echo "EOF" >>"$GITHUB_ENV"
