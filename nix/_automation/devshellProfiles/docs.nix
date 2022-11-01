7ff911cc60a63cda0ceceaca931f47563425f7b3
GTrunSec <gtrunsec@hardenedlinux.org>
2022-11-01 13:14:30 -0700

    fix

--- nix/_automation/devshellProfiles/docs.nix
+++ nix/_automation/devshellProfiles/docs.nix
@@ -2,7 +2,7 @@
   commands = [
     {
       name = "mkdoc";
-      command = "nix run $PRJ_ROOT#x86_64-linux.command.entrypoints.mkdoc -- $@";
+      command = "nix run $PRJ_ROOT#x86_64-linux.common.entrypoints.mkdoc -- $@";
       help = "mkdoc with org-roam-book";
       category = "docs";
     }
--- nix/_automation/devshellProfiles/std.nix
+++ nix/_automation/devshellProfiles/std.nix
@@ -9,9 +9,6 @@
 }: let
   withCategory = category: attrset: attrset // {inherit category;};
 in {
-  std.docs.enable = false;
-  std.adr.enable = false;
-
   imports = [
     inputs.std.std.devshellProfiles.default
     "${extraModulesPath}/git/hooks.nix"
