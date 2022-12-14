a816f192c83d74d3085467e6740990dc166688cc
GTrunSec <gtrunsec@hardenedlinux.org>
2022-12-14 02:03:32 -0800

    refactor: rewrite writeComoniconApplication

--- nix/writers/writeComoniconApplication.nix
+++ nix/writers/writeComoniconApplication.nix
@@ -3,20 +3,29 @@
   cell,
 }: {
   name,
-  julia ? inputs.cells.comonicon.packages.julia-wrapped,
+  julia ? {
+    package = inputs.cells.comonicon.packages.julia-wrapped;
+    pure = false;
+  },
   path ? "",
   args ? [],
   runtimeEnv ? {},
   threads ? 8,
   runtimeInputs ? [],
+  pure ? false,
 }: let
   inherit (cell) lib;
+  l = inputs.nixpkgs.lib // builtins;
 in
   lib.writeShellApplication {
     inherit name;
     runtimeEnv = runtimeEnv;
-    runtimeInputs = [julia] ++ runtimeInputs;
-    text = ''
-      julia -e "import Pkg; Pkg.activate(\"${path}\"); Pkg.instantiate();" -L ${path}/${builtins.concatStringsSep " " args} "$@"
-    '';
+    runtimeInputs = runtimeInputs;
+    text =
+      l.optionalString julia.pure ''
+        ${l.getExe julia.package} ${path}/${builtins.concatStringsSep " " args} "$@"
+      ''
+      + l.optionalString (!julia.pure) ''
+        ${l.getExe julia.package} -e "import Pkg; Pkg.activate(\"${path}\"); Pkg.instantiate();" -L julia ${path}/${builtins.concatStringsSep " " args} "$@"
+      '';
   }
