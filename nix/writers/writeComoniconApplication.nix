04801162377d87fb79124bf6516d70e3b7ac60e6
GTrunSec <gtrunsec@hardenedlinux.org>
2022-12-21 02:27:11 -0800

    Update org-roam

--- nix/writers/writeComoniconApplication.nix
+++ nix/writers/writeComoniconApplication.nix
@@ -26,6 +26,6 @@ in
         ${l.getExe julia.package} ${path}/${builtins.concatStringsSep " " args} "$@"
       ''
       + l.optionalString (!julia.pure) ''
-        ${l.getExe julia.package} -e "import Pkg; Pkg.activate(\"${path}\"); Pkg.instantiate();" -L julia ${path}/${builtins.concatStringsSep " " args} "$@"
+        ${l.getExe julia.package} -e "import Pkg; Pkg.activate(\"${path}\"); Pkg.instantiate();" -L ${path}/${builtins.concatStringsSep " " args} "$@"
       '';
   }
