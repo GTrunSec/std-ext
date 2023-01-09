23e1a869abb2726a7bf7490cd74bdd6503ef15f4
guangtao <gtrunsec@hardenedlinux.org>
2023-01-09 03:56:05 -0800

    fix nvfetche

--- nix/update/packages/default.nix
+++ nix/update/packages/default.nix
@@ -3,7 +3,7 @@
   cell,
 }: let
   inherit (inputs.cells.common.lib) __inputs__;
-  nvfetcher = __inputs__.nvfetcher.defaultPackage;
+  nvfetcher = __inputs__.nvfetcher.packages.default;
 in {
   inherit nvfetcher;
 }
