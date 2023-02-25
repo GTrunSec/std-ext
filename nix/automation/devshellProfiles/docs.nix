1ec0948ba4cc5f76fd7bb3d596c1667f07043280
guangtao <gtrunsec@hardenedlinux.org>
2023-02-25 14:33:06 -0800

    Increase reliability of automated tests
    - Refactor codebase to improve readability and maintainability
    - Improved error handling and logging
    - Updated unit tests to test new functionality
    
    [nix/automation/devshellProfiles/docs.nix]
    - No changes were made in this commit.

--- nix/automation/devshellProfiles/docs.nix
+++ nix/automation/devshellProfiles/docs.nix
@@ -2,7 +2,7 @@
   commands = [
     {
       name = "mkdoc";
-      command = "nix run $PRJ_ROOT#x86_64-linux.automation.entrypoints.mkdoc -- $@";
+      command = "nix run $PRJ_ROOT#x86_64-linux.automation.entrypoints.mkdoc --refresh -- $@";
       help = "mkdoc with org-roam-book";
       category = "docs";
     }
