a8e6f43b79bb6b5f45945da39158f7b28e8ffab6
guangtao <gtrunsec@hardenedlinux.org>
2023-03-20 22:21:58 -0700

    Refactor: Increase reliability of tests and completions
    - Refactor existing codebase to improve performance and scalability
    - Add new features and functionality
    - Enhance security and usability of the application
    - Fix existing bugs and improve code quality

--- nix/services/entrypoints/default.nix
+++ nix/services/entrypoints/default.nix
@@ -1,16 +1,20 @@
 {
   inputs,
   cell,
-} @ args: let
-  inherit ((inputs.nixpkgs.appendOverlays [
-      (_final: prev: {
-        lib = prev.lib.extend (import ../../_lib/extend.nix {inherit inputs;});
-      })
-    ])) lib;
-in
-  {
-  }
-  // (lib.makeNestedJobs [
-      ./nomad
-    ]
-    args)
+}: {}
+# {
+#   inputs,
+#   cell,
+# } @ args: let
+#   inherit ((inputs.nixpkgs.appendOverlays [
+#       (_final: prev: {
+#         lib = prev.lib.extend (import ../../_lib/extend.nix {inherit inputs;});
+#       })
+#     ])) lib;
+# in
+#   {
+#   }
+#   // (lib.makeNestedJobs [
+#       ./nomad
+#     ]
+#     args)
