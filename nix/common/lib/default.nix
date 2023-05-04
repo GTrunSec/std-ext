a2933208f806050ea4a31128590ea63f187c14d2
guangtao <gtrunsec@hardenedlinux.org>
2023-05-04 09:32:14 -0700

    fix: callInputs system

--- nix/common/lib/default.nix
+++ nix/common/lib/default.nix
@@ -4,8 +4,10 @@
 }: let
   inherit (inputs) nixpkgs std flops;
 
-  callInputs = (flops.lib.flake.pops.default.setInitInputs (flops.lib.callFlake ./lock))
-    .setSystem "x86_64-linux";
+  callInputs =
+    (flops.lib.flake.pops.default.setInitInputs (flops.lib.callFlake ./lock))
+    .setSystem
+    nixpkgs.system;
 in {
   inherit callInputs;
   __inputs__ = callInputs.outputsForInputsCompat;
